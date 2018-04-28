8ba274b8b34899e996920e47bab9e2452e4c8b7f
jethro <jethrozhu@qq.com>
2018-04-28 16:34:29 +0800

    添加了动态点赞，图片点赞，图片收藏，动态收藏接口

--- src/main/java/com/doubles/controller/ImageController.java
+++ src/main/java/com/doubles/controller/ImageController.java
@@ -1,9 +1,15 @@
 package com.doubles.controller;
 
 
+import com.doubles.entity.Article;
+import com.doubles.entity.Image;
+import com.doubles.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -17,5 +23,15 @@ import org.springframework.stereotype.Controller;
 @RequestMapping("/image")
 public class ImageController {
 
+    @Autowired
+    ImageService imageService;
+    @RequestMapping("/addImgLike")
+    public void addLike(HttpServletRequest request, String articleId){
+        Image image = imageService.selectById(articleId);
+        if(image !=  null){
+            image.setLikeNumber(image.getLikeNumber()+1);
+            imageService.updateById(image);
+        }
+    }
 }
 
